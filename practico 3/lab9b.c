a71022375321e693b912071aab0ebc9137b24b9c
Fernando Lara <larafernandoagustin@gmail.com>
2024-10-28 15:22:38 -0300

    Update lab9b.c

--- practico 3/lab9b.c
+++ practico 3/lab9b.c
@@ -10,14 +10,14 @@ int main(void) {
     scanf("%d", &c);
     i = 0;
     c = 0;
-    printf("Ingresa el valor de A.0\n");
-    scanf("%d", &A[0]);
-    printf("Ingresa el valor de A.1\n");
-    scanf("%d", &A[1]);
-    printf("Ingresa el valor de A.2\n");
-    scanf("%d", &A[2]);
-    printf("Ingresa el valor de A.3\n");
-    scanf("%d", &A[3]);
+    int i2;
+    i2 = 0;
+    while (i2 < 4)
+    {
+        printf("Ingresa el valor de A.%i\n", i2);
+        scanf("%d", &A[i2]);
+        i2 = i2+1;
+    }
     while (i<4)
     {
         if (A[i]>0)
