8a460a7bd1c113e9a3b0d1aeb22dc96f82a8fe70
Fernando Lara <larafernandoagustin@gmail.com>
2025-03-20 17:25:15 -0300

    Update fixstring.c

--- practico4/lab01-kickstart (2)/lab01/ej0/fixstring.c
+++ practico4/lab01-kickstart (2)/lab01/ej0/fixstring.c
@@ -4,23 +4,34 @@
 #include "fixstring.h"
 
 unsigned int fstring_length(fixstring s) {
-    /*
-     * COMPLETAR
-     *
-     */
+    unsigned int i;
+    i=0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return i;
+    
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    bool is_eq = true;
+
+    int i = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0') {
+        i++;
+    }
+    is_eq = s1[i] == s2[i];
+    return is_eq;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    bool res = true;
+    int i = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0') {
+        i++;
+    }
+    res = s1[i] <= s2[i];
+    return res;
 }
-
