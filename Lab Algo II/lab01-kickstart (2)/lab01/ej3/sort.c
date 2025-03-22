6a135822c04ce164dadaab2a8c19c0de93d59cd0
Fernando Lara <larafernandoagustin@gmail.com>
2025-03-22 12:13:58 -0300

    lab 1, ejercicio 3

--- Lab Algo II/lab01-kickstart (2)/lab01/ej3/sort.c
+++ Lab Algo II/lab01-kickstart (2)/lab01/ej3/sort.c
@@ -9,22 +9,41 @@
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
-
-    /* PRECONDITION: 
-       0 <= izq < der < length of the array
-
-     Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
-    */
+    unsigned int i, j, ppiv;
+    ppiv = izq;
+    i = izq +1;
+    j = der;
+    while (i <= j)
+    {
+        if (goes_before(a[i], a[ppiv])) 
+        {
+            i++;
+        } else if (goes_before(a[ppiv], a[j]))
+        {
+            j--;
+        } else if (a[i] > a[ppiv] && a[j] < a[ppiv])
+        {
+            swap(a, i, j);
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
+    return ppiv;
+    
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
-
+    unsigned int ppiv;
+    array_dump(a, der+1);
+    if(der > izq) {
+        array_dump(a, der+1);
+        ppiv = partition(a, izq, der);
+        array_dump(a, der+1);
+        quick_sort_rec(a, izq, ppiv-1);
+        quick_sort_rec(a, ppiv+1, der);
+    }
 }
+
 
 void quick_sort(int a[], unsigned int length) {
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
