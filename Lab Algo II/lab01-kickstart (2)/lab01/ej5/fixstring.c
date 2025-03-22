8a219f8aaea3cba1efca43685bacbbbed7429423
Fernando Lara <larafernandoagustin@gmail.com>
2025-03-22 16:35:30 -0300

    lab 1, ejercicio 5 (con segmentation fault)

--- Lab Algo II/lab01-kickstart (2)/lab01/ej5/fixstring.c
+++ Lab Algo II/lab01-kickstart (2)/lab01/ej5/fixstring.c
@@ -5,20 +5,39 @@
 
 
 unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    unsigned int i;
+    i=0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return i;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool is_eq = true;
+
+    int i = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0') {
+        i++;
+    }
+    is_eq = s1[i] == s2[i];
+    return is_eq;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool res = true;
+    int i = 0;
+    while (i<FIXSTRING_MAX && s1[i] == s2[i] && s2[i] != '\0') {
+        i++;
+    }
+    res = s1[i] <= s2[i];
+    return res;
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
     int i=0;
-    while (i<FIXSTRING_MAX && s2[i]!='\0') {
+    while (i<FIXSTRING_MAX && s2[i] != '\0') {
         s1[i] = s2[i];
         i++;
     }
@@ -27,10 +46,9 @@ void fstring_set(fixstring s1, const fixstring s2) {
 
 void fstring_swap(fixstring s1,  fixstring s2) {
     fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    fstring_set(aux, s1);
+    fstring_set(s1, s2);
+    fstring_set(s2, aux);
 }
 
 
--- Lab Algo II/lab01-kickstart (2)/lab01/ej5/sort.c
+++ Lab Algo II/lab01-kickstart (2)/lab01/ej5/sort.c
@@ -10,11 +10,40 @@
 
 
 static unsigned int partition(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int i = izq + 1, j = der, ppiv = izq;
+
+    while (i <= j) {
+        // Mueve i hacia la derecha mientras a[i] sea menor que el pivote
+        if (i <= der && goes_before(a[i], a[ppiv])) {
+            i++;
+        }
+        // Mueve j hacia la izquierda mientras a[j] sea mayor que el pivote
+        else if (j > izq && goes_before(a[ppiv], a[j])) {
+            j--;
+        }
+        // Si los elementos a[i] y a[j] están fuera de lugar, los intercambiamos
+        else {
+            swap(a, i, j);
+        }
+    }
+
+    // Colocamos el pivote en su posición correcta
+    swap(a, ppiv, j);
+
+    return j; // Retornamos la posición del pivote
 }
+    
 
 static void quick_sort_rec(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int ppiv;
+    array_dump(a, der+1);
+    if(der > izq) {
+        array_dump(a, der+1);
+        ppiv = partition(a, izq, der);
+        array_dump(a, der+1);
+        quick_sort_rec(a, izq, ppiv-1);
+        quick_sort_rec(a, ppiv+1, der);
+    }
 }
 
 void quick_sort(fixstring a[], unsigned int length) {
