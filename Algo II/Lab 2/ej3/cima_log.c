dd4e480726f1408d4fa8355656169b96f11d6f0c
Fernando Lara <larafernandoagustin@gmail.com>
2025-04-05 22:44:48 -0300

    Ejercicio 3 terminado

---
+++ Algo II/Lab 2/ej3/cima_log.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "cima_log.h"
+
+/**
+ * @brief Dado un arreglo que tiene cima, devuelve la posición de la cima
+ * usando la estrategia divide y venceras.
+ *
+ * Un arreglo tiene cima si existe una posición k tal que el arreglo es
+ * estrictamente creciente hasta la posición k y estrictamente decreciente
+ * desde la posición k.
+ * La cima es el elemento que se encuentra en la posición k.
+ * PRECONDICION: tiene_cima(a, length)
+ *
+ * @param a Arreglo.
+ * @param length Largo del arreglo.
+ */
+
+
+int cima_log_rec(int a[], int lft, int rgt) {
+    int pos;
+    int mid;
+    if(lft>rgt) {
+        pos = 0;
+    } else {
+        mid = (lft+rgt) / 2;
+        if(a[mid-1] < a[mid] && a[mid] < a[mid+1]) {
+            pos = cima_log_rec(a, mid+1, rgt);
+        } else if(a[mid-1] < a[mid] && a[mid]> a[mid+1]) {
+            pos = mid;
+        } else {
+            pos = cima_log_rec(a, lft, mid-1);
+        }
+    }
+
+    return pos;
+}
+
+int cima_log(int a[], int length) {
+   int pos = cima_log_rec(a, 0, length-1);
+   return pos;
+}
+
---
+++ Algo II/Lab 2/ej3/tests.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "cima_log.h"
+
+#define MAX_LENGTH 10
+#define N_TESTCASES_CIMA 10
+
+int main() {
+    struct testcase {
+        int a[MAX_LENGTH];
+        int length;
+        int result;
+    };
+
+    struct testcase tests[N_TESTCASES_CIMA] = {
+        { {8}, 1, 0 },                              // único elemento
+        { {1, 2, 3, 2, 1}, 5, 2 },                  // cima en 2
+        { {1, 3, 2}, 3, 1 },                        // cima en 1
+        { {1, 5, 9, 7, 6, 2}, 6, 2 },               // cima en 2
+        { {1, 2, 5, 4, 1}, 5, 2 },                  // cima en 2
+        { {2, 4, 6, 9, 7, 4, 3}, 7, 3 },            // cima en 3
+        { {3, 6, 10, 8, 7, 5, 1}, 7, 2 },           // cima en 2
+        { {1, 3, 7, 6, 4, 2, 0}, 7, 2 },            // cima en 2
+        { {4, 5, 6, 9, 8, 4}, 6, 3 },               // cima en 3
+        { {10}, 1, 0 }                              // único elemento
+    };
+    int result;
+
+    printf("TESTING cima\n");
+
+    for (int i=0; i < N_TESTCASES_CIMA; i++) {
+        printf("Test case %i: ", i+1);
+
+        result = cima_log(tests[i].a, tests[i].length);
+
+        if (result == tests[i].result) {
+            printf("OK\n");
+        } else {
+            printf("FAILED. got %d expected %d\n", result, tests[i].result);
+        }
+    }
+
+    return 0;
+}
